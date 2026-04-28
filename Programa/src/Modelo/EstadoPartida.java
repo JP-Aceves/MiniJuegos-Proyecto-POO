955c4fa6673e789bf53791394941819fba7b76db
JoseAceves1 <josepabloaceves@hotmail.com>
2026-04-28 18:46:09 +0200

    feat: implementar enum EstadoPartida con Javadoc

---
+++ Programa/src/Modelo/EstadoPartida.java
@@ -0,0 +1,17 @@
+package Modelo;
+
+/**
+ * Enum que representa los posibles estados de una partida.
+ *
+ * @author JP-Aceves
+ * @version 1.0
+ */
+public enum EstadoPartida {
+    /** La partida está en curso. */
+    EN_CURSO,
+    /** La partida está pausada. */
+    PAUSADA,
+    /** La partida ha finalizado. */
+    FINALIZADA
+}
+
