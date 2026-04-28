d04c3c50a922693ef819aa73095209d4e56b5e10
JoseAceves1 <josepabloaceves@hotmail.com>
2026-04-28 18:20:23 +0200

    feat: añadir atributos, constructor y métodos a PuntuacionJugador

--- Programa/src/Modelo/PuntuacionJugador.java
+++ Programa/src/Modelo/PuntuacionJugador.java
@@ -1,5 +1,48 @@
 package Modelo;
 
+/**
+ * Representa la puntuación de un jugador dentro de una partida.
+ * Cada instancia almacena el username del jugador y sus puntos acumulados.
+ */
 public class PuntuacionJugador {
 
+    private String username;
+    private int puntos;
+
+    /**
+     * Crea una nueva puntuación para el jugador indicado, con 0 puntos iniciales.
+     *
+     * @param username nombre de usuario del jugador
+     */
+    public PuntuacionJugador(String username) {
+        this.username = username;
+        this.puntos = 0;
+    }
+
+    /**
+     * Devuelve el nombre de usuario del jugador.
+     *
+     * @return username del jugador
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Devuelve los puntos actuales del jugador.
+     *
+     * @return puntos acumulados
+     */
+    public int getPuntos() {
+        return puntos;
+    }
+
+    /**
+     * Incrementa los puntos del jugador en la cantidad indicada.
+     *
+     * @param n cantidad de puntos a sumar
+     */
+    public void sumarPuntos(int n) {
+        this.puntos += n;
+    }
 }
