531944ef3e57f42ee4f7324c75889f403d62443c
adrian-duque <adrian.duqueluengo@gmail.com>
2026-05-02 11:20:10 +0200

    fix:Arreglo Clase Estadisticas

--- Programa/src/Modelo/Estadistica.java
+++ Programa/src/Modelo/Estadistica.java
@@ -2,13 +2,13 @@ package Modelo;
 
 /**
  * Clase que representa las estadísticas de una partida jugada.
- * 
+ *
  * Almacena la información relativa a una partida incluyendo datos del jugador,
  * el juego jugado, la puntuación obtenida, el resultado de la partida y la fecha.
- * 
+ *
  * Esta clase proporciona métodos para acceder a los datos de la estadística
  * y convertirlos a diferentes formatos (CSV para almacenamiento y texto para visualización).
- * 
+ *
  * @author juancarlos
  * @version 1.0
  */
@@ -16,22 +16,22 @@ public class Estadistica {
 
     /** El nombre de usuario del jugador que participó en la partida */
     private String username;
-    
+
     /** El nombre del juego que se jugó */
     private String nombreJuego;
-    
+
     /** La puntuación obtenida en la partida */
     private int puntuacion;
-    
+
     /** Indica si la partida fue ganada (true) o perdida (false) */
     private boolean victoria;
-    
+
     /** La fecha en que se jugó la partida */
     private String fecha;
 
     /**
      * Construye una nueva estadística de partida con los datos especificados.
-     * 
+     *
      * @param username el nombre de usuario del jugador
      * @param nombreJuego el nombre del juego jugado
      * @param puntuacion la puntuación obtenida en la partida
@@ -48,7 +48,7 @@ public class Estadistica {
 
     /**
      * Obtiene el nombre de usuario del jugador.
-     * 
+     *
      * @return el nombre de usuario como String
      */
     public String getUsername() {
@@ -57,7 +57,7 @@ public class Estadistica {
 
     /**
      * Obtiene el nombre del juego que se jugó.
-     * 
+     *
      * @return el nombre del juego como String
      */
     public String getNombreJuego() {
@@ -66,7 +66,7 @@ public class Estadistica {
 
     /**
      * Obtiene la puntuación obtenida en la partida.
-     * 
+     *
      * @return la puntuación como int
      */
     public int getPuntuacion() {
@@ -75,7 +75,7 @@ public class Estadistica {
 
     /**
      * Obtiene el resultado de la partida.
-     * 
+     *
      * @return true si la partida fue ganada, false si fue perdida
      */
     public boolean isVictoria() {
@@ -84,7 +84,7 @@ public class Estadistica {
 
     /**
      * Obtiene la fecha en que se jugó la partida.
-     * 
+     *
      * @return la fecha como String
      */
     public String getFecha() {
@@ -93,9 +93,9 @@ public class Estadistica {
 
     /**
      * Convierte la estadística a formato CSV para almacenamiento en archivo.
-     * 
+     *
      * El formato de la línea CSV es: username,nombreJuego,puntuacion,victoria,fecha
-     * 
+     *
      * @return una línea en formato CSV con todos los datos de la estadística
      */
     public String toArchivo() {
@@ -104,24 +104,24 @@ public class Estadistica {
 
     /**
      * Devuelve una representación textual de la estadística para mostrar en pantalla.
-     * 
+     *
      * El formato de la salida es:
      * Usuario: &lt;username&gt; | Juego: &lt;nombreJuego&gt; | Puntuación: &lt;puntuacion&gt; | Resultado: &lt;resultado&gt; | Fecha: &lt;fecha&gt;
-     * 
+     *
      * Donde el resultado muestra:
      * <ul>
      *   <li>"VICTORIA, Enhorabuena" si la partida fue ganada</li>
      *   <li>"DERROTA, Sigue intentandolo" si la partida fue perdida</li>
      * </ul>
-     * 
+     *
      * @return una representación textual formateada de la estadística
      */
     @Override
     public String toString() {
         String resultado = victoria ? "VICTORIA, Enhorabuena" : "DERROTA, Sigue intentandolo";
         return String.format(
-            "Usuario: %s | Juego: %s | Puntuación: %d | Resultado: %s | Fecha: %s",
-            username, nombreJuego, puntuacion, resultado, fecha
+                "Usuario: %s | Juego: %s | Puntuación: %d | Resultado: %s | Fecha: %s",
+                username, nombreJuego, puntuacion, resultado, fecha
         );
     }
 }
