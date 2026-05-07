1b7a57df544db5eac7e19bfc3ebbe499ac2d4f67
JoseAceves1 <josepabloaceves@hotmail.com>
2026-05-07 19:37:41 +0200

    feat: implementar clase abstracta VentanaJuego

--- Programa/src/Vista/VentanaJuego.java
+++ Programa/src/Vista/VentanaJuego.java
@@ -1,5 +1,43 @@
 package Vista;
+import Controlador.GestorPartidas;
+import Controlador.GestorEstadisticas;
+import javax.swing.*;
 
-public class VentanaJuego {
+public abstract class VentanaJuego extends JFrame {
+    protected GestorPartidas gestorPartidas;
+    protected GestorEstadisticas gestorEstadisticas;
+    protected JFrame ventanaPadre;
+
+    public VentanaJuego(JFrame ventanaPadre, GestorPartidas gestorPartidas, GestorEstadisticas gestorEstadisticas) {
+        this.ventanaPadre = ventanaPadre;
+        this.gestorPartidas = gestorPartidas;
+        this.gestorEstadisticas = gestorEstadisticas;
+    }
+
+    public abstract void actualizarVista();
+
+    protected void accionPausar() {
+        gestorPartidas.pausarPartida();
+        JOptionPane.showMessageDialog(this, "Partida pausada y guardada.");
+        dispose();
+        ventanaPadre.setVisible(true);
+    }
+    
+    protected void accionFinalizar() {
+        String fecha = obtenerFechaHoy();
+        gestorEstadisticas.registrarResultado(gestorPartidas.getPartidaActual(), fecha);
+        gestorPartidas.finalizarPartida();
+        dispose();
+        ventanaPadre.setVisible(true);
+    }
+    
+    protected String obtenerFechaHoy() {
+        java.util.Calendar cal = java.util.Calendar.getInstance();
+        return cal.get(java.util.Calendar.DAY_OF_MONTH) + "/" +
+               (cal.get(java.util.Calendar.MONTH) + 1) + "/" +
+               cal.get(java.util.Calendar.YEAR);
+    }
 
 }
+
+
